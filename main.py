e9c6a51c85319e5dee9146d9553e400049805c47
SHIV7899 <65497526+SHIV7899@users.noreply.github.com>
2021-04-25 03:35:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,59 +9,20 @@ import multiprocessing
 import threading
 import concurrent
 
-BOT_OWNER_ROLE = '//STAR KING//' # change to what you need
-#BOT_OWNER_ROLE_ID = "733205382704398379"
+BOT_OWNER_ROLE = 'Y.A.S.P. BOT' # change to what you need
+#BOT_OWNER_ROLE_ID = "834718012740861952"
   
  
 
  
 oot_channel_id_list = [              
-	"755690433346273281",#hq-answer
-    "593990608914219008", #loco galaxy
-	"607613349491900436", #loco IQ
-    "569420128794443776", #loco unt
-    "569502072945377290", #indian loco
-	"595635734904307742", #tf loco
-	"612177236107460618",#sani loco
-	"591498350562377741",#planet loco
-	"605443517069656084", #tf confetti
-	"593990638916075520", #galaxy confett
-	"590583414541910018",# confetti IQ
-	"591294134564683809", #indian confetti
-	"588070986554015764",#unt confetti
-	"609405529575653387",# kingdom confetti
-	"612177284471717894",#sani confetti
-	"591498756562878475",#planet confetti
-	"595639586726740049",#tf hq
-	"591068955523809328",#hq galaxy
-	"580198028950896640",#HQ tribe
-        "459842150323060736",#hq dimensions
-        "513818250652680213",#hq world
-        "569420198717816852",#hq unt
-	"568617830258442255"#hq revolution
-	"598669844983840779",#cashquiz dimension
-	"446448458090545172",#cashquiz tribe
-	"610713322509303809",#cashquiz galaxy
-	"595639664300392472",#cashquiz tf
-	"596527077402869770",#theq tf
-	"501220538518077440",#theq dimensions
-	"446448458090545172",#theq tribe
-	"513818839008673833",#theq world
-	"569420278006808586",#theq unt
-	"580208596139245601",#theq revolution
-	"535675285211971584",#swagIQ world
-	"595639769904447502",#swagIQ tf
-	"446448437119025154",#swagIQ tribe
-	"501220306128601098",#swagIQ dimension
-	"570794448808837131",#swagIQ revolution
-	"514915010955313153",#confeti vietnam world
-	"595640787933331466",#confetti vietnam tf
-	"501219307477532674",#confeti vietnam dimension
-	"571241319658291200",#confeti vietnam unt
-	"609003338675126272",#confetti vietnam pride
-	"611439844996153375",#confetti mexico pride
-	"611980037243273220",#confettimexico pride
-	"611751492054941696",#confetti mexico
+	"831030092641599498"
+	"831030094377254972"
+	"835790083692167209"
+	"832650978268020859"
+	"828651669415133244"
+	"826542982958284857"
+	"801499863417225217"
 ]
 
 
@@ -113,7 +74,7 @@ class SelfBot(discord.Client):
 
     async def on_ready(self):
         print("======================")
-        print("Nelson Trivia Self Bot")
+        print("NIGHTMARE BOT")
         print("Connected to discord.")
         print("User: " + self.user.name)
         print("ID: " + str(self.user.id))
@@ -171,7 +132,7 @@ class Bot(discord.Client):
         self.answer_scores = answer_scores
 
         # embed creation
-        self.embed=discord.Embed(title="**__TRIVIA SAVAGE | PRO__**", description="**Web Searching** :spy:")
+        self.embed=discord.Embed(title="⚠//Y.A.S.P.//⚠POWER⚠", description="**Web Searching** :spy:")
         self.embed.set_author(name ='',url=' ',icon_url='https://images-ext-2.discordapp.net/external/aMZ8_Dhu3Cib5U1l--xzP6QVgEV6bzjPDLMC-gNawWY/https/cdn.discordapp.com/attachments/577373201164795904/585046581506605076/ezgif-2-2f5a82b8174f.gif?width=225&height=225')
         self.embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/595713706411819033/604679180201754674/image0.png")
         self.embed.add_field(name="Option I", value="0", inline=False)
@@ -248,7 +209,7 @@ class Bot(discord.Client):
 
     async def on_ready(self):
         print("==============")
-        print("Nelson Trivia")
+        print("NIGHT MARE BOT")
         print("Connected to discord.")
         print("User: " + self.user.name)
         print("ID: " + str(self.user.id))
@@ -256,7 +217,7 @@ class Bot(discord.Client):
         await self.clear_results()
         await self.update_embeds()
         await self.change_presence(activity=discord.Game(name='with '+str(len(set(self.get_all_members())))+' users'))
-        await self.change_presence(activity=discord.Game(name='Trivia with DARKKNIGHT||*help'))
+        await self.change_presence(activity=discord.Game(name='Trivia with ADMINISTRATIVE *help'))
 
     async def on_message(self, message):
 
@@ -282,8 +243,8 @@ class Bot(discord.Client):
           await message.delete()
           if BOT_OWNER_ROLE in [role.name for role in message.author.roles]:
            embed = discord.Embed(title="Help Commands", description="**How Run Bot**", color=0x00ff00)
-           embed.add_field(name="Support Game", value="**Loco\nConfetti-India\nFlipkart\nJeetoh\nHQ Trivia\nCashquiz\nSwag IQ\nThe Q\nConfetti Vietnam\nConfetti mexico**", inline=False)
-           embed.add_field(name="when Question come put command", value=" `*` **is command work for all support game except**\n**`*j` is command of jeetoh**\n**`*f` is command for filpkart**\n\n**use cmd! in particular channels**\n\n**FOR MORE INFO CONTACT TO CAPTAIN COOL#0044**", inline=False)
+           embed.add_field(name="Support Game", value="**HQ Trivia**", inline=False)
+           embed.add_field(name="when Question come put command", value=" `.H` is command work for all support game\n**FOR MORE INFO CONTACT TO //SIMBHA//#3664**", inline=False)
            await message.channel.send(embed=embed)
           
 
@@ -311,7 +272,7 @@ def bot_with_cyclic_update_process(update_event, answer_scores):
     upd_thread.start()
 
     loop = asyncio.get_event_loop()
-    loop.create_task(bot.start('NzU3ODQ4MDA3NTg3NzkwODc5.X2mW9Q.hlRWhm-lQgLgiOMGCFBoytPgQXA'))
+    loop.create_task(bot.start('NzIyODY0MTQzMTUzMjM0MDUx.X3uygw.J-NCospMLYMHNZG9QtxHTGb24jY'))
     loop.run_forever()
 
 
@@ -320,7 +281,7 @@ def selfbot_process(update_event, answer_scores):
     selfbot = SelfBot(update_event, answer_scores)
 
     loop = asyncio.get_event_loop()
-    loop.create_task(selfbot.start('NjU4ODYzMzg2OTc2NTgzNjgw.X2mUJQ.YSI8mwG7ybxih_mY2hEvrV_h6JU'))
+    loop.create_task(selfbot.start('ODM1ODI2MjcyMTg1NjE0MzU2.YIVF4A.32cryB2tLYZj7J9B15Tq3L9GZco'))
                                    
     loop.run_forever()
 
